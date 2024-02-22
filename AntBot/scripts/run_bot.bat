69dfcec10ec606f8f925e79039db5a449e3ce26a
Under_Scroll <tifabval@gmail.com>
2024-02-22 13:03:09 +0100

    Fix script paths
    When the path had spaces it broke

--- AntBot/scripts/run_bot.bat
+++ AntBot/scripts/run_bot.bat
@@ -1,10 +1,10 @@
 @echo off
 
-mkdir %~dp0..\..\tools\bots
+mkdir "%~dp0..\..\tools\bots"
 
-move %~dp0..\bin\x64\Release\AntBot.exe %~dp0..\..\tools\bots
+move "%~dp0..\bin\x64\Release\AntBot.exe" "%~dp0..\..\tools\bots"
 
-cd %~dp0..\..\tools
+cd "%~dp0..\..\tools"
 
 cmd /k "play_one_game_mybot.cmd"
 
